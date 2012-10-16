b3ef21fa30a13af3c5328db546a6bcd1db6be952
thomas <tpalissier@gmail.com>
2012-10-16 10:37:14 +0200

    Lol

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class test{
+
+
+public static void main (String[] args) {
+int n=0;
+	for (int i=0; i<=1000;i++){
+		if (i%3==0 || i%5==0)
+			n=+i;
+	}
+System.out.println(n);
+
+
+}
+}
+
